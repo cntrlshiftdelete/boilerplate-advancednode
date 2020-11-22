49c38d703c4ffee16bb8300d486ad428986c5f5b
repl.it user <replituser@example.com>
2020-11-22 13:19:22 +0000

    :sparkles::wrench:Added pug

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug');
 
-fccTesting(app); //For FCC testing purposes
+fccTesting(app); // For fCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  // Change the response to render the Pug template
+  res.render(process.cwd() + '/views/pug');
 });
 
 app.listen(process.env.PORT || 3000, () => {
